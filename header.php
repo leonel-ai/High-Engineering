d5d1792c8ad965428da3495c25773a894c51632e
leonel.ai <leonelaguzman1@gmail.com>
2019-01-25 13:44:08 -0600

    center nav items

--- header.php
+++ header.php
@@ -27,101 +27,107 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 
-    <nav class="navbar navbar-expand-md navbar-light bg-transparent">
-      <a class="navbar-brand" href="/">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png"
-          width="170" height="80" class="d-inline-block align-top" alt="High Engineering logo"/>
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <nav class="navbar navbar-expand-md navbar-light fixed-top bg-white">
+      <div class="mx-auto d-md-flex d-block flex-md-nowrap">
+        <a class="navbar-brand" href="/">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png"
+            width="170" height="80" class="d-inline-block align-top" alt="High Engineering logo"/>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarMenu">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item dropdown">
-            <button class="btn btn-primary btn-md dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Products
-            </button>
-            <div class="dropdown-menu dropdown-multicol" aria-labelledby="navbarDropdown">
-              <div class="dropdown-col">
-                <a class="dropdown-item" href="#">Oranges</a>
-                <a class="dropdown-item" href="#">Bananas</a>
-                <a class="dropdown-item" href="#">Apples</a>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item dropdown">
+              <button class="btn btn-primary btn-md dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Products
+              </button>
+              <div class="dropdown-menu dropdown-multicol" aria-labelledby="navbarDropdown">
+                <div class="dropdown-row">
+                  <a class="dropdown-item text-center" href="#">Products Overview</a>
+                </div>
+                <div class="dropdown-col">
+                  <a class="dropdown-item" href="#">Oranges</a>
+                  <a class="dropdown-item" href="#">Bananas</a>
+                  <a class="dropdown-item" href="#">Apples</a>
+                </div>
+                <div class="dropdown-col">
+                  <a class="dropdown-item" href="#">Potatoes</a>
+                  <a class="dropdown-item" href="#">Leeks</a>
+                  <a class="dropdown-item" href="#">Cauliflowers</a>
+                </div>
+                <div class="dropdown-col">
+                  <a class="dropdown-item" href="#">Beef</a>
+                  <a class="dropdown-item" href="#">Pork</a>
+                  <a class="dropdown-item" href="#">Venison</a>
+                </div>
               </div>
-              <div class="dropdown-col">
-                <a class="dropdown-item" href="#">Potatoes</a>
-                <a class="dropdown-item" href="#">Leeks</a>
-                <a class="dropdown-item" href="#">Cauliflowers</a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <button class="btn btn-primary btn-md dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Services
+              </button>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
               </div>
-              <div class="dropdown-col">
-                <a class="dropdown-item" href="#">Beef</a>
-                <a class="dropdown-item" href="#">Pork</a>
-                <a class="dropdown-item" href="#">Venison</a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <button class="btn btn-primary btn-md dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Applications
+              </button>
+              <div class="dropdown-menu dropdown-menu-right dropdown-multicol" aria-labelledby="navbarDropdown">
+                <div class="dropdown-row">
+                  <a class="dropdown-item text-center" href="#">Applications Overview</a>
+                </div>
+                <div class="dropdown-col">
+                  <a class="dropdown-item" href="#">Oranges</a>
+                  <a class="dropdown-item" href="#">Bananas</a>
+                  <a class="dropdown-item" href="#">Apples</a>
+                </div>
+                <div class="dropdown-col">
+                  <a class="dropdown-item" href="#">Potatoes</a>
+                  <a class="dropdown-item" href="#">Leeks</a>
+                  <a class="dropdown-item" href="#">Cauliflowers</a>
+                </div>
+                <div class="dropdown-col">
+                  <a class="dropdown-item" href="#">Beef</a>
+                  <a class="dropdown-item" href="#">Pork</a>
+                  <a class="dropdown-item" href="#">Venison</a>
+                </div>
               </div>
-            </div>
-          </li>
+            </li>
 
-          <li class="nav-item dropdown">
-            <button class="btn btn-primary btn-md dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Services
-            </button>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-
-          <li class="nav-item dropdown">
-            <button class="btn btn-primary btn-md dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Applications
-            </button>
-            <div class="dropdown-menu dropdown-menu-right dropdown-multicol" aria-labelledby="navbarDropdown">
-              <div class="dropdown-col">
-                <a class="dropdown-item" href="#">Oranges</a>
-                <a class="dropdown-item" href="#">Bananas</a>
-                <a class="dropdown-item" href="#">Apples</a>
+            <li class="nav-item dropdown">
+              <button class="btn btn-primary btn-md dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Resources
+              </button>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
               </div>
-              <div class="dropdown-col">
-                <a class="dropdown-item" href="#">Potatoes</a>
-                <a class="dropdown-item" href="#">Leeks</a>
-                <a class="dropdown-item" href="#">Cauliflowers</a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <button class="btn btn-primary btn-md dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                About
+              </button>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
               </div>
-              <div class="dropdown-col">
-                <a class="dropdown-item" href="#">Beef</a>
-                <a class="dropdown-item" href="#">Pork</a>
-                <a class="dropdown-item" href="#">Venison</a>
-              </div>
-            </div>
-          </li>
-
-          <li class="nav-item dropdown">
-            <button class="btn btn-primary btn-md dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Resources
-            </button>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-
-          <li class="nav-item dropdown">
-            <button class="btn btn-primary btn-md dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              About
-            </button>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-
-
-        </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
 

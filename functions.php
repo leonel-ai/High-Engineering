58ee6526c38c7b15be239a5513e541b8305974a5
leonel.ai <leonelaguzman1@gmail.com>
2019-02-13 15:48:04 -0600

    add quote css to functions

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@
       wp_enqueue_style( 'bootstrap.min', get_stylesheet_directory_uri() . '/css/bootstrap.min.css' );
       wp_enqueue_style( 'menu', get_stylesheet_directory_uri(). '/css/menu.css', array(), rand(111,9999), 'all');
       wp_enqueue_style( 'form', get_stylesheet_directory_uri(). '/css/form.css', array(), rand(111,9999), 'all');
+      wp_enqueue_style( 'quote', get_stylesheet_directory_uri(). '/css/quote.css', array(), rand(111,9999), 'all');
 		wp_enqueue_style( 'ap-google-fonts', 'http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700|Open+Sans:300,400,400i,600,700,800', false );
     }
     add_action( 'wp_enqueue_scripts', 'themebs_enqueue_styles');
@@ -16,7 +17,7 @@
       wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . '/js/vendor/bootstrap.bundle.min.js', array( 'jquery' ) );
       wp_enqueue_script('custom-scripts', get_stylesheet_directory_uri() . '/js/custom-scripts.js', array( 'jquery' ), rand(111,9999), true);
 	wp_enqueue_script('ap-custom-scripts', get_stylesheet_directory_uri() . '/js/ashley-custom-scripts.js', array( 'jquery' ), rand(111,9999), true);
-		 
+
     }
     add_action( 'wp_enqueue_scripts', 'themebs_enqueue_scripts');
 
@@ -26,7 +27,7 @@ function tags_tinymce_fix( $init )
 {
   // html elements being stripped
   $init['extended_valid_elements'] = 'span[*], a[*]';
-  
+
   return $init;
 }
 add_filter('tiny_mce_before_init', 'tags_tinymce_fix');
